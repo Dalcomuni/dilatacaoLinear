d096d0886d35645e4993d09428044bbea01c49b1
Ricardo Dalcomuni <161725206+Dalcomuni@users.noreply.github.com>
2024-09-02 11:41:16 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@C-1/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("##################\n");
+  printf("#dilatacao linear#\n");
+  printf("##################\n");
+
+  
+  printf("identifique o coeficiente da dilatacao linear c:\n");
+  float coeficienteLinear;
+  scanf("%f", &coeficienteLinear);
+
+  
+  printf("identifique a tempetaruta inicial em c°:\n");
+  float temperaturaInicial;
+  scanf("%f", &temperaturaInicial);
+
+
+  printf("identifique a temperatura final:\n");
+  float temperaturaFinal;
+  scanf("%f", &temperaturaFinal);
+
+  float variaçãoDeTemperatura = temperaturaFinal - temperaturaInicial;
+
+  printf
+  
+  
+  return 0;
+}
---
+++ .ccls-cache/@home@runner@Dilatacao-Linear/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("##################\n");
+  printf("#dilatacao linear#\n");
+  printf("##################\n");
+
+  
+  printf("identifique o coeficiente da dilatacao linear c:\n");
+  float coeficienteLinear;
+  scanf("%f", &coeficienteLinear);
+
+  
+  printf("identifique a tempetaruta inicial em c°:\n");
+  float temperaturaInicial;
+  scanf("%f", &temperaturaInicial);
+
+
+  printf("identifique a temperatura final:\n");
+  float temperaturaFinal;
+  scanf("%f", &temperaturaFinal);
+
+  float variaçãodeTemperatura = temperaturaFinal - temperaturaInicial;
+
+  printf("Indique o comprimento inicial do material:\n");
+  float comprimentoInicial;
+  scanf("%f" ,&comprimentoInicial);
+
+  float VariacaodeComprimento = comprimentoInicial * variaçãodeTemperatura;
+  printf("a variacao de comprimento é:");
+  printf("%f" ,VariacaodeComprimento);
+
+  return 0;
+}
